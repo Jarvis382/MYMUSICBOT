c44556672878900b0f7a3f65e2f3fb216cf21d36
Ashutosh Avinash Madhav <58766301+Jarvis382@users.noreply.github.com>
2020-07-21 11:14:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const { prefix } = require("./config.json");
 const ytdl = require("ytdl-core");
-
+client.login(process.env.TOKEN);
 const client = new Discord.Client();
 
 const queue = new Map();
